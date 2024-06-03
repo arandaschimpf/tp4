1158f840901cffb6bf825953046edf932eddfe25
Mateo Maldonado <mateo.maldonado@uap.edu.ar>
2024-06-03 09:18:03 -0300

    consigna a terminada

---
+++ tp4/src/services/contadores.ts
@@ -0,0 +1,49 @@
+'use server'
+import { PrismaClient } from "@prisma/client"
+import { create } from "domain"
+
+const prisma = new PrismaClient()
+
+export type Contador = {
+    id: string
+    numero: number
+}
+export async function getContadores() {
+  return await prisma.contadores.findMany()
+}
+
+export async function aumentarContador(id: string) {
+  return await prisma.contadores.update({
+    where: { id },
+    data: {
+      numero: {
+        increment: 1
+      }
+    }
+  })
+}
+
+export async function disminuirContador(id: string) {
+  return await prisma.contadores.update({
+    where: { id },
+    data: {
+      numero: {
+        decrement: 1
+      }
+    }
+  })
+}
+
+export async function createContador() {
+    return await prisma.contadores.create({
+        data: {
+        numero: 0
+        }
+    })
+}
+
+export async function getContador(id: string) {
+    return await prisma.contadores.findUnique({
+        where: { id }
+    })
+}
