6b8a1b752d58301eb38d5d75b585d398a0d7cadf
Korosensee <francisco.girard@uap.edu.ar>
2024-06-03 21:21:24 -0300

    Tp-4_deploy

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className=" bg-slate-400" >{children}</body>
+    </html>
+  );
+}
---
+++ src/contador/contador.tsx
@@ -0,0 +1,36 @@
+//El subdominio en vercel es: https://tp4-despliegue.vercel.app/
+"use client"
+import { incrementarN, resetN } from "@/services/number";
+import { useState } from "react";
+
+function Contador() {
+  const [but, setBut] = useState(0)
+
+  async function incrementar() {
+    let x = await incrementarN();
+    setBut(x)
+  }
+  async function reset() {
+    let x = await resetN();
+    setBut(x)
+  }
+  return (
+    <div className=" p-6 justify-center items-center flex flex-col">
+      <button     
+
+        onClick={incrementar}    
+      >
+        Contador: {but}
+      </button>
+      <button
+      onClick={reset}   
+      >
+      Resetear
+      </button>
+
+   </div>
+    
+  );
+}
+
+export default Contador;
